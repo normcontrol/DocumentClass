228c4ad1525c871b6c76f393ff9661c718425185
Anddrei V. Berreezhkov <dead0343@gmail.com>
2023-02-15 18:13:12 +0300

    Add ENUM IS_UNKNOWN state

--- EnumFill.py
+++ EnumFill.py
@@ -1,6 +1,7 @@
 import enum
 from enum import Enum
 
+
 @enum.unique
 class EnumFill(Enum):
     """
@@ -10,7 +11,9 @@ class EnumFill(Enum):
     NO_APPLY - property does not occur in the element
     APPLY_TO_ALL_ELEMENTS -  property is applied to the entire element
     APPLY_TO_SOME_ELEMENTS -  property is applied to a part of the element (unknown which one)
+    IS_UNKNOWN -  It is not known whether the property is applied in any form to the text
     """
     NO_APPLY = 0
     APPLY_TO_ALL_ELEMENTS = 1
     APPLY_TO_SOME_ELEMENTS = 2
+    IS_UNKNOWN = None
